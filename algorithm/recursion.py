7328f85ffc74faf3a58d7d12997037e96a0800ac
unknown <tjenian@outlook.com>
2024-01-07 13:41:10 +0800

    start graphs

--- algorithm/recursion.py
+++ algorithm/recursion.py
@@ -285,7 +285,8 @@ def dynamic_knapsack(weight_cap, weights, values):
   # Return the value of the bottom right of matrix
   return matrix[rows-1][weight_cap]
 
-
+# longest common subsequence is the series of letters from left to right two strings share
+# e.g. longest sequence is "ABAD" given "ABAZDC" and "BACBAD"
 def longest_common_subsequence(string_1, string_2):
   print("Finding longest common subsequence of {0} and {1}".format(string_1, string_2))
   # For the longest common subsequence, we’ll need a grid where the columns are 
@@ -293,7 +294,31 @@ def longest_common_subsequence(string_1, string_2):
   # We’ll add an extra column and row to represent an empty string or “no character”.
   grid = [[0 for col in range(len(string_1)+1)] for row in range(len(string_2)+1)]
   
+  for row in range(1, len(string_2)+1):
+    print("Comparing: {0}".format(string_2[row - 1]))
+    for col in range(1, len(string_1) + 1):
+      print("Against: {0}".format(string_1[col - 1]))
+      if string_1[col - 1] == string_2[row - 1]:
+        grid[row][col] = grid[row - 1][col - 1] + 1
+      else:
+        grid[row][col] = max(grid[row - 1][col], grid[row][col - 1])
+  
+  for row_line in grid:
+    print(row_line)
 
+  # construct subsequence value
+  result = []
+  while row > 0 and col > 0:
+    if string_1[col - 1] == string_2[row - 1]:
+      result.append(string_1[col - 1])
+      row -= 1
+      col -= 1
+    elif grid[row - 1][col] > grid[row][col - 1]:
+      row -= 1
+    else:
+      col -= 1
+  result.reverse()
+  return "".join(result)
 
 
 # construct a binary search tree
